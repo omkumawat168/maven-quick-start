867b4256ea3e8a7329a517aa3502514c7eff640f
TestGit <omkumawat168@gmail.com>
2018-12-19 15:09:39 +0530

    pom.xml and Application.java is modified

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,5 +1,6 @@
 //Adding a sample application class
 package clinic.programming.training;
+import java.util.*;
 
 public class Application{
 		
@@ -7,9 +8,20 @@ public class Application{
 		System.out.println("Dummy App");
 	}
 
+	public void greet(){
+		List<String> list = new ArrayList<>();
+		list.add("Hello");
+		list.add("World");
+
+		for(String s:list){
+			System.out.println(s);
+		}
+	}
+
 	public static void main(String[] args){
 		System.out.println("This is a sample application.");
 		Application app = new Application();
+		app.greet();
 
 	}
 }
