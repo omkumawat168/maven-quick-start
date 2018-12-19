41e0d187d9b0b6f22f863aadb36b21966bb7f43f
TestGit <omkumawat168@gmail.com>
2018-12-19 13:46:29 +0530

    adding Application.java

---
+++ src/main/java/clinic/programming/training/Application.java
@@ -0,0 +1,16 @@
+//Adding a sample application class
+package clinic.programming.training;
+
+public class Application{
+		
+	public Application(){
+		System.out.println("Dummy App");
+	}
+
+	public static void main(String[] args){
+		System.out.println("This is a sample application.");
+		Application app = new Application();
+
+	}
+}
+
