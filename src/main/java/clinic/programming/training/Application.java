c5b1bb6222ba9f605ff5a79fba5986440d967e5d
TestGit <omkumawat168@gmail.com>
2018-12-19 17:24:14 +0530

    StringUtils Added in Application.java

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,11 +1,19 @@
 //Adding a sample application class
 package clinic.programming.training;
 import java.util.*;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application{
 		
 	public Application(){
 		System.out.println("Dummy App");
+	}
+
+	public int countWords(String para){
+	
+		String[] seperateWords = StringUtils.split(para,' ');
+		return (seperateWords==null)?0:seperateWords.length;
+
 	}
 
 	public void greet(){
@@ -22,6 +30,7 @@ public class Application{
 		System.out.println("This is a sample application.");
 		Application app = new Application();
 		app.greet();
+		System.out.println("Total Words:"+app.countWords("I have four words."));
 
 	}
 }
